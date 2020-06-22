c0d5a996e777f8ed5cdd3d21e8c9e63104ab92c0
zhangaz1 <zhangaz1@hotmail.com>
2020-06-22 16:23:05 +0800

    spaceShip3 temp

--- src/pages/spaceShip3/enemy.ts
+++ src/pages/spaceShip3/enemy.ts
@@ -19,6 +19,59 @@ const canDestroy = r.both(isDead, hasNoShots);
 const shouldKeep = r.complement(canDestroy);
 
 export function createEnemiesStream(refresh$: rx.Observable<number>, config$: rx.Observable<IConfig>): rx.Observable<ITimestampData<IEnemy[]>> {
+	let enemies = [] as IEnemy[];
+
+	const enemySource = config$.pipe(
+		rxo.map((config: IConfig) => rx.interval(config.enemyFreq)),
+		rxo.switchAll(),
+		rxo.withLatestFrom(config$),
+		rxo.map(createEnemy)
+	).subscribe((enemy: IEnemy) => {
+		enemies.push(enemy);
+	});
+
+	const enemiesShots = config$.pipe(
+
+	);
+
+	const movingEnemies = refresh$.pipe(
+		rxo.withLatestFrom(config$),
+		rxo.map(moveEnemies),
+		rxo.map(r.filter(shouldKeep)) ,
+	).subscribe(es => enemies = es);
+
+	return refresh$.pipe(
+		rxo.map(refresh => {
+			return {
+				timestamp: refresh,
+				data: enemies,
+			};
+		}),
+	);
+
+	function moveEnemies(crb: any) {
+		const [refresh, config] = crb as [number, IConfig];
+		return r.forEach((enemy: IEnemy) => {
+			enemy.shots = [...enemy.shotsBuffer];
+
+			if (isAlive(enemy)) {
+				enemy.y += config.enemySpeed;
+				enemy.x += getRandomInt(-config.enemyWidthRange, config.enemyWidthRange);
+
+				if (enemy.x < 0) {
+					enemy.x += config.width;
+				}
+				if (enemy.x > config.width) {
+					enemy.x -= config.width;
+				}
+				if (enemy.y > config.height) {
+					enemy.y -= config.height;
+				}
+			}
+		})(enemies);
+	}
+
+
 	const enemySource$ = config$.pipe(
 		rxo.map(config => rx.interval(config.enemyFreq)),
 		rxo.switchAll(),
@@ -96,27 +149,6 @@ export function createEnemiesStream(refresh$: rx.Observable<number>, config$: rx
 	return withTimestamp$.pipe(rxo.share());
 }
 
-function moveEnemies(crb: any) {
-	const [enemies, enemies2, config] = crb as [IEnemy[], IEnemy[], IConfig];
-	return r.forEach((enemy: IEnemy) => {
-		enemy.shots = [...enemy.shotsBuffer];
-
-		if (isAlive(enemy)) {
-			enemy.y += config.enemySpeed;
-			enemy.x += getRandomInt(-config.enemyWidthRange, config.enemyWidthRange);
-
-			if (enemy.x < 0) {
-				enemy.x += config.width;
-			}
-			if (enemy.x > config.width) {
-				enemy.x -= config.width;
-			}
-			if (enemy.y > config.height) {
-				enemy.y -= config.height;
-			}
-		}
-	})(enemies);
-}
 
 function createEnemy(crb: any): IEnemy {
 	const [n, config] = crb as [number, IConfig];
